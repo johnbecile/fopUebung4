b37988f6b0129521be49d233c43a2c2d6a6c1c7b
Hao Liu <liuhaodeu@gmail.com>
2023-11-22 04:08:53 +0100

    cloned from Uebung 4

---
+++ src/main/java/h04/Main.java
@@ -0,0 +1,27 @@
+package h04;
+
+import static org.tudalgo.algoutils.student.Student.crash;
+
+/**
+ * Main entry point in executing the program.
+ */
+public class Main {
+    /**
+     * Main entry point in executing the program.
+     *
+     * @param args program arguments, currently ignored
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+//        main01();
+//        main02();
+    }
+
+    public static void main01() {
+        crash();
+    }
+
+    public static void main02() {
+        crash();
+    }
+}
